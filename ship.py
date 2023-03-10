848070df2ae1fe3262d4e44f4c9560f42f8e2443
Adamant <zt.gaming.sh@gmail.com>
2023-03-10 16:26:14 +0200

    Playing field limitation and refactoring _check_events

--- ship.py
+++ ship.py
@@ -28,9 +28,9 @@ class Ship:
     def update(self):
         """Updates the ship's position based on the "Movement"."""
         # Updates attribute x.
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
 
         # Updating the rect attribute based on self.x.
