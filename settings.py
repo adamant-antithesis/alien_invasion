c27e73573087d46182f61a7670e6a28593728af7
Adamant <zt.gaming.sh@gmail.com>
2023-03-10 16:09:16 +0200

    Adding left-right movement

--- settings.py
+++ settings.py
@@ -6,4 +6,4 @@ class Settings:
         # Screen parameters
         self.screen_width = 1200
         self.screen_height = 750
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (19, 69, 138)
--- ship.py
+++ ship.py
@@ -17,6 +17,17 @@ class Ship:
         # Each new ship appears at the bottom of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Movement
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Updates the ship's position based on the "Movement"."""
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
+
     def blitme(self):
         """Draws the ship at the current position."""
         self.screen.blit(self.image, self.rect)
