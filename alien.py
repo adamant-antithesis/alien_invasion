9a63ef0c0994d827cd7d4e2b8615d408a73c530f
Adamant <zt.gaming.sh@gmail.com>
2023-03-13 13:36:23 +0200

    Adding alien movement

--- alien.py
+++ alien.py
@@ -9,6 +9,7 @@ class Alien(Sprite):
         """Initializes the alien and sets its starting position."""
         super().__init__()
         self.screen = ai_game.screen
+        self.settigs = ai_game.settings
 
         # Load the alien image and assign the rect attribute.
         self.image = pygame.image.load('images/alien.png')
@@ -21,3 +22,14 @@ class Alien(Sprite):
 
         # Save the exact horizontal position of the alien.
         self.x = float(self.rect.x)
+
+    def check_edges(self):
+        """Returns True if the alien is at the edge of the screen."""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
+
+    def update(self):
+        """Moves the alien to the right."""
+        self.x += (self.settigs.alien_speed * self.settigs.fleet_direction)
+        self.rect.x = self.x
--- settings.py
+++ settings.py
@@ -12,8 +12,14 @@ class Settings:
         self.ship_speed = 1.5
 
         # Bullets parameters
-        self.bullet_speed = 1
+        self.bullet_speed = 2
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 5
+
+        # Alien parameters
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction = 1 indicates movement to the right; and -1 - to the left.
+        self.fleet_direction = 1
